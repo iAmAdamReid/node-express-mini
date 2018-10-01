eb9ee86587eb751c61a87482f413ab260aff1d63
iAmAdamReid <adam@adamreid.me>
2018-10-01 11:31:57 -0700

    Express pulls users JSON data from db API on localhost:8000/api/users

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // implement your API here
+// how to import / export code between files
+// introduce how routing works=
+
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('<h1>Hello, FSW13!</h1>')
+});
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        console.log(`\n users`, users);
+        res.json(users)
+    })
+    .catch(error => res.send(error));
+})
+
+const port = 8000;
+server.listen(port, () => {
+    console.log(`\n API running on ${port} === \n`);
+});
